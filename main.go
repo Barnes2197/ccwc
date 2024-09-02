62175cdc90864dc9cca4b0c3cf5604aaa3e268e5
Barnes2197 <mauriceb2197@gmail.com>
2024-09-02 11:51:24 -0700

    Added cobra and run a root command

--- main.go
+++ main.go
@@ -3,11 +3,30 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+
+	var rootCmd = &cobra.Command{
+		Use:   "myapp",
+		Short: "MyApp is a simple CLI application",
+		Long:  `MyApp is a longer description of your CLI application.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// This is what happens when no subcommands are specified
+			readBytes("testfiles/test.txt")
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func readBytes(filepath string) {
+	file, err := os.Open(filepath)
 
 	if err != nil {
 		panic(err)
@@ -27,5 +46,4 @@ func main() {
 	}
 
 	fmt.Println(len(content))
-
 }
