81622f12555e836d4a9ead376b3247ad21dc4570
Barnes2197 <mauriceb2197@gmail.com>
2024-09-02 22:48:13 -0700

    Finished project, now supports all flags wc supports. Was not completely tested so bugs can exist

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
+
+var countBytes, countLines, countWords, countCharacters bool
 
 func main() {
 
@@ -13,11 +17,70 @@ func main() {
 		Use:   "myapp",
 		Short: "MyApp is a simple CLI application",
 		Long:  `MyApp is a longer description of your CLI application.`,
+		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			// This is what happens when no subcommands are specified
-			readBytes("testfiles/test.txt")
+			file, err := os.Open(args[0])
+
+			if err != nil {
+				panic(err)
+			}
+
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+			var content []string
+			for scanner.Scan() {
+				// Retrieve the text of the current line
+				nextLine := scanner.Text()
+
+				// Process the line (for this example, just print it)
+				content = append(content, nextLine)
+
+			}
+
+			lineCount := 0
+
+			for _, line := range content {
+				if line != "" {
+					lineCount++
+				}
+			}
+
+			if countLines {
+				printOutput(lineCount, args[0])
+				return
+			}
+
+			if countBytes {
+				printOutput(getByteCount(content), args[0])
+				return
+			}
+
+			if countWords {
+				printOutput(getWordCount(content), args[0])
+				return
+			}
+
+			if countCharacters {
+				printOutput(getChracterCount(content), args[0])
+				return
+			}
+
+			wordCount := getWordCount(content)
+			byteCount := getByteCount(content)
+
+			output := fmt.Sprintf("%d %d %d %s", lineCount, wordCount, byteCount, args[0])
+
+			fmt.Println(output)
+
 		},
 	}
+
+	rootCmd.Flags().BoolVarP(&countBytes, "count-bytes", "c", false, "Output the byte count of a text file")
+	rootCmd.Flags().BoolVarP(&countLines, "count-lines", "l", false, "Output the line count of a text file")
+	rootCmd.Flags().BoolVarP(&countWords, "count-words", "w", false, "Output the word count of a text file")
+	rootCmd.Flags().BoolVarP(&countCharacters, "count-characters", "m", false, "Output the character count of a text file")
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -25,25 +88,39 @@ func main() {
 	}
 }
 
-func readBytes(filepath string) {
-	file, err := os.Open(filepath)
+func printOutput(count int, filepath string) {
+	fmt.Println(fmt.Sprintf("%d %s", count, filepath))
+}
 
-	if err != nil {
-		panic(err)
+func getByteCount(content []string) int {
+	byteCount := 0
+	for _, line := range content {
+		if line != "" {
+			byteCount += len(line) + 1
+		}
 	}
+	return byteCount
+}
 
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var content []byte
-	for scanner.Scan() {
-		// Retrieve the text of the current line
-		fileBytes := scanner.Bytes()
-
-		// Process the line (for this example, just print it)
-		content = append(content, fileBytes...)
-
+func getWordCount(content []string) int {
+	wordCount := 0
+	for _, line := range content {
+		words := strings.Split(line, " ")
+		sum := 0
+		for _, word := range words {
+			if word != "" {
+				sum += 1
+			}
+		}
+		wordCount += sum
 	}
+	return wordCount
+}
 
-	fmt.Println(len(content))
+func getChracterCount(content []string) int {
+	charCount := -1
+	for _, line := range content {
+		charCount += utf8.RuneCountInString(line) + 1
+	}
+	return charCount
 }
